1a0ed753fbfcfa8522c6385f704bca14a1e34cb2
Angel Kasabov <ankasabov142@gmail.com>
2023-08-15 21:26:03 +0300

    Updated styles and added snowing animation

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+/* <div class="row foxVideo">
+          <video
+            src="/resources/Encounter a young wild white Arctic Fox.mp4"
+          ></video>
+        </div> */
 //Accesing the video and the div that contains it
 const intro = document.querySelector(".foxVideo");
 const video = intro.querySelector("video");
@@ -6,17 +11,18 @@ const video = intro.querySelector("video");
 const controller = new ScrollMagic.Controller();
 
 const scene = new ScrollMagic.Scene({
-  duration: 10000,
+  duration: 9000,
   triggerElement: intro,
   triggerHook: 0,
 })
   .setPin(intro)
   .addTo(controller);
+
 let accelamount = 0.1;
 let srollpos = 0;
-let delay = 0;
+let delay = 0.5;
 
-scene.on("update", e => {
+scene.on("update", (e) => {
   srollpos = e.scrollPos / 1000;
 });
 
@@ -24,3 +30,40 @@ setInterval(() => {
   delay += (srollpos - delay) * accelamount;
   video.currentTime = delay;
 }, 33.3);
+
+//creating snow animation
+const snowContainer = document.querySelector(".snow-container");
+const snowContent = ["&#10052", "&#10053", "&#10054"];
+
+const random = (num) => {
+  return Math.floor(Math.random() * num);
+};
+
+const getRandomStyles = () => {
+  const top = random(100);
+  const left = random(100);
+  const dur = random(10) + 10;
+  const size = random(25) + 25;
+  return `
+    top: -${top}%;
+    left: ${left}%;
+    font-size: ${size}px;
+    animation-duration: ${dur}s;
+  `;
+};
+
+const createSnow = (num) => {
+  for (var i = num; i > 0; i--) {
+    var snow = document.createElement("div");
+    snow.className = "snow";
+    snow.style.cssText = getRandomStyles();
+    snow.innerHTML = snowContent[random(3)];
+    snowContainer.append(snow);
+  }
+};
+
+window.addEventListener("load", () => {
+  createSnow(60);
+});
+
+
